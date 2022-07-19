dce7f71a1b2d7fe54036434922c5fba786daa7c4
Pouya Vedadiyan <vedadiyan@gmail.com>
2022-07-19 15:40:49 +0800

    Quick Fixes

--- Natx.Client/src/EncodedConnection.cs
+++ Natx.Client/src/EncodedConnection.cs
@@ -1,73 +1,119 @@
-using NATS.Client;
-
 namespace Natx.Client;
 
-public sealed class EncodedConnection
+public abstract class EncodedConnection : Natx.Abstraction.IEncodedConnection
 {
-    public IConnection Connection { get; }
-    private Natx.Abstraction.IEncodedConnection encodedConnection;
-    public EncodedConnection(Natx.Abstraction.IEncodedConnection encodedConnection)
+    public NATS.Client.IConnection Connection { get; }
+    public EncodedConnection()
     {
-        Connection = new ConnectionFactory().CreateConnection();
-        this.encodedConnection = encodedConnection;
+        Connection = new NATS.Client.ConnectionFactory().CreateConnection();
     }
     public void Publish(string subject, object data)
     {
-        byte[] bytes = encodedConnection.Encode(data);
+        byte[] bytes = Encode(data);
         Connection.Publish(subject, bytes);
     }
     public void Publish(Msg msg)
     {
-        msg.Message.Data = encodedConnection.Encode(msg.Data);
+        msg.Message.Data = Encode(msg.Data);
         Connection.Publish(msg.Message);
     }
     public void Publish(string subject, string reply, object data)
     {
-        byte[] bytes = encodedConnection.Encode(data);
+        byte[] bytes = Encode(data);
         Connection.Publish(subject, reply, bytes);
     }
     public Msg Request(string subject, object data)
     {
-        byte[] bytes = encodedConnection.Encode(data);
+        byte[] bytes = Encode(data);
         NATS.Client.Msg msg = Connection.Request(subject, bytes);
-        return new Msg(msg, encodedConnection.Decode(msg.Data));
+        return new Msg(msg, Decode(msg.Data));
     }
     public Msg Request(Msg msg)
     {
-        msg.Message.Data = encodedConnection.Encode(msg.Data);
+        msg.Message.Data = Encode(msg.Data);
         NATS.Client.Msg response = Connection.Request(msg.Message);
-        return new Msg(response, encodedConnection.Decode(response.Data));
+        return new Msg(response, Decode(response.Data));
     }
     public Msg Request(Msg msg, TimeSpan timeout)
     {
-        msg.Message.Data = encodedConnection.Encode(msg.Data);
+        msg.Message.Data = Encode(msg.Data);
         NATS.Client.Msg response = Connection.Request(msg.Message, (int)timeout.TotalMilliseconds);
-        return new Msg(response, encodedConnection.Decode(response.Data));
+        return new Msg(response, Decode(response.Data));
     }
     public Msg Request(string subject, object data, TimeSpan timeout)
     {
-        byte[] bytes = encodedConnection.Encode(data);
+        byte[] bytes = Encode(data);
         NATS.Client.Msg msg = Connection.Request(subject, bytes, (int)timeout.TotalMilliseconds);
-        return new Msg(msg, encodedConnection.Decode(msg.Data));
+        return new Msg(msg, Decode(msg.Data));
     }
     public SyncSubscription SubscribeSync(string subject)
     {
-        ISyncSubscription syncSubscription = Connection.SubscribeSync(subject);
-        return new SyncSubscription(encodedConnection, syncSubscription);
+        NATS.Client.ISyncSubscription syncSubscription = Connection.SubscribeSync(subject);
+        return new SyncSubscription(this, syncSubscription);
     }
     public SyncSubscription SubscribeSync(string subject, string queue)
     {
-        ISyncSubscription syncSubscription = Connection.SubscribeSync(subject, queue);
-        return new SyncSubscription(encodedConnection, syncSubscription);
+        NATS.Client.ISyncSubscription syncSubscription = Connection.SubscribeSync(subject, queue);
+        return new SyncSubscription(this, syncSubscription);
     }
     public AsyncSubscription SubscribeAsync(string subject)
     {
-        IAsyncSubscription asyncSubscription = Connection.SubscribeAsync(subject);
-        return new AsyncSubscription(encodedConnection, asyncSubscription);
+        NATS.Client.IAsyncSubscription asyncSubscription = Connection.SubscribeAsync(subject);
+        return new AsyncSubscription(this, asyncSubscription);
     }
     public AsyncSubscription SubscribeAsync(string subject, string queue)
     {
-        IAsyncSubscription asyncSubscription = Connection.SubscribeAsync(subject, queue);
-        return new AsyncSubscription(encodedConnection, asyncSubscription);
+        NATS.Client.IAsyncSubscription asyncSubscription = Connection.SubscribeAsync(subject, queue);
+        return new AsyncSubscription(this, asyncSubscription);
+    }
+
+    public abstract object Decode(byte[] data);
+    public abstract byte[] Encode(object data);
+}
+
+public class ServerTemplate
+{
+    private readonly EncodedConnection encodedConnection;
+    private readonly AsyncSubscription subscription;
+    private readonly Func<object, object> handler;
+    public ServerTemplate(EncodedConnection encodedConnection, Func<object, object> handler)
+    {
+        this.encodedConnection = encodedConnection;
+        subscription = encodedConnection.SubscribeAsync("", "");
+        this.handler = handler;
+        subscription.MessageHandler += onMessage;
+    }
+    private void onMessage(object? sender, MsgHandlerEventArgs e)
+    {
+        try
+        {
+            object response = handler(e.Message.Data);
+            e.Message.Respond(response);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    public void Start()
+    {
+        subscription.Start();
+    }
+    public void Stop()
+    {
+        subscription.Unsubscribe();
+    }
+}
+
+public class ClientTemplate
+{
+    private readonly EncodedConnection encodedConnection;
+    public ClientTemplate(EncodedConnection encodedConnection)
+    {
+        this.encodedConnection = encodedConnection;
+    }
+    public object Request(object request)
+    {
+        return encodedConnection.Request("", request, TimeSpan.FromMinutes(1));
     }
 }
--- Natx.Client/src/Msg.cs
+++ Natx.Client/src/Msg.cs
@@ -3,9 +3,21 @@ public readonly struct Msg
 {
     public NATS.Client.Msg Message { get; }
     public object Data { get; }
+    private readonly EncodedConnection? encodedConnection;
     public Msg(NATS.Client.Msg Message, object data)
     {
         this.Message = Message;
         Data = data;
+        encodedConnection = null;
+    }
+    public Msg(NATS.Client.Msg Message, object data, EncodedConnection? encodedConnection)
+    {
+        this.Message = Message;
+        Data = data;
+        this.encodedConnection = encodedConnection;
+    }
+    public void Respond(object data)
+    {
+        Message.Respond(encodedConnection?.Encode(data));
     }
 }
